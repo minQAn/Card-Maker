a39dbac3f532f5de5424ae2c025c8b17f7ed231b
minQ <minq.an7@gmail.com>
2022-08-09 14:23:10 -0400

    add login component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.module.css';
-import App from './App';
+import App from './app';
+import AuthService from './service/auth.service';
+
+const authService = new AuthService();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App authService={authService} />
   </React.StrictMode>
 );
--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -1,11 +1,12 @@
 import firebase from 'firebase';
+import firebaseApp from './firebase'; // to use the initialized app
 
 // firebase Docs
 // https://firebase.google.com/docs/auth/web/google-signin
 class AuthService {
   login(providerName) {
     const authProvider = new firebase.auth[`${providerName}AuthProvider`]();
-    return firebase.auth().signInWithPopup(authProvider); // Promise
+    return firebaseApp.auth().signInWithPopup(authProvider); // Promise
   }
 }
 
